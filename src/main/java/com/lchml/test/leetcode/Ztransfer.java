173860403b122ab9b5757a4b03a0a8a402832689
hzhemeiling <hzhemeiling@corp.netease.com>
2018-05-13 22:36:17 +0800

    first import

---
+++ src/main/java/com/lchml/test/leetcode/Ztransfer.java
@@ -0,0 +1,45 @@
+package com.lchml.test.leetcode;
+
+public class Ztransfer {
+    public static void main(String[] args) {
+        System.out.println(convert("ABCDE", 4));
+    }
+
+    public static String convert(String s, int numRows) {
+        if (s == null || s.length() <= 1 || numRows == 1 || s.length() == numRows) {
+            return s;
+        }
+        char[] chars = s.toCharArray();
+        int tmp = chars.length / (numRows + numRows - 2);
+        int tmpMode = chars.length % (numRows + numRows - 2);
+        int numCols = tmp + tmp * (numRows - 2) + tmpMode % numRows + 1;
+        char[][] z = new char[numRows][numCols];
+        int idxRow = 0;
+        int idxCol = 0;
+        for (int i=0; i<chars.length; i++) {
+            if (idxCol % (numRows - 1) == 0) {
+                z[idxRow][idxCol] = chars[i];
+                idxRow++;
+                if (idxRow == numRows) {
+                    idxRow -= 2;
+                    idxCol++;
+                }
+            } else {
+                z[idxRow][idxCol] = chars[i];
+                idxRow--;
+                idxCol++;
+            }
+        }
+
+        char[] ret = new char[chars.length];
+        int idxRet = 0;
+        for (int i=0; i<numRows; i++) {
+            for (int j=0; j<numCols; j++) {
+                if (z[i][j] != '\u0000') {
+                    ret[idxRet++] = z[i][j];
+                }
+            }
+        }
+        return new String(ret);
+    }
+}
