5ee38d935f58e669c982eab68e9c8071ecd12035
licheng <licheng_xd@163.com>
2019-02-18 18:17:26 +0800

    clean

---
+++ src/main/java/com/lchml/test/leetcode/GenerateParenthesis_22.java
@@ -0,0 +1,32 @@
+package com.lchml.test.leetcode;
+
+import java.util.List;
+
+/**
+ * 给出 n 代表生成括号的对数，请你写出一个函数，使其能够生成所有可能的并且有效的括号组合。
+ *
+ * 例如，给出 n = 3，生成结果为：
+ *
+ * [
+ *   "((()))",
+ *   "(()())",
+ *   "(())()",
+ *   "()(())",
+ *   "()()()"
+ * ]
+ *
+ * Created by lc on 2019/02/18.
+ */
+public class GenerateParenthesis_22 {
+
+	// 回溯算法
+	public List<String> generateParenthesis(int n) {
+		return null;
+	}
+
+
+
+	public static void main(String[] args) {
+
+	}
+}
--- src/main/java/com/lchml/test/leetcode/RemoveNthFromEnd_19.java
+++ src/main/java/com/lchml/test/leetcode/RemoveNthFromEnd_19.java
@@ -1,15 +1,24 @@
 package com.lchml.test.leetcode;
 
 /**
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ *
+ * 示例：
+ *
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ *
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ * 说明：
+ *
+ * 给定的 n 保证是有效的。
+ *
+ * 进阶：
+ *
+ * 你能尝试使用一趟扫描实现吗？
+ *
  * Created by lc on 2019/02/15.
  */
 public class RemoveNthFromEnd_19 {
-
-	public static class ListNode {
-	 	int val;
-        ListNode next;
-	    ListNode(int x) { val = x; }
-	}
 
 	// 普通两次遍历方法
 	public ListNode removeNthFromEnd(ListNode head, int n) {
@@ -61,6 +70,6 @@ public class RemoveNthFromEnd_19 {
 	}
 
 	public static void main(String[] args) {
-		new RemoveNthFromEnd_19().removeNthFromEnd(new ListNode(1), 1);
+		System.out.println(new RemoveNthFromEnd_19().removeNthFromEnd(ListNode.fromInts(1, 2, 3, 4, 5), 2));
 	}
 }
